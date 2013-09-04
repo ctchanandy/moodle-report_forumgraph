c402d30668685347d1a4432ca124b4cb6ba6a3e5
Andy Chan <ctchan.andy@gmail.com>
2013-09-04 15:37:00 +0800

    Add the function to extends the navigation with the report items

--- lib.php
+++ lib.php
@@ -221,3 +221,17 @@ function report_forumgraph_create_json($nodes, $edges, $uid_mapping) {
     $json .= '}';
     return $json;
 }
+
+/**
+ * This function extends the navigation with the report items
+ *
+ * @param navigation_node $navigation The navigation node to extend
+ * @param stdClass $course The course to object for the report
+ * @param stdClass $context The context of the course
+ */
+function report_forumgraph_extend_navigation_course($navigation, $course, $context) {
+    if (has_capability('report/forumgraph:view', $context)) {
+        $url = new moodle_url('/report/forumgraph/index.php', array('course'=>$course->id));
+        $navigation->add(get_string('pluginname', 'report_forumgraph'), $url, navigation_node::TYPE_SETTING, null, null, new pix_icon('i/report', ''));
+    }
+}
